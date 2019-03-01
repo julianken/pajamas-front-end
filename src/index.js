85b3da28b229d3d54c45a1ed5d3f1c5063f0ad7a
Julian <julian.kennon.d@gmail.com>
2019-02-28 21:43:53 -0800

    add react and webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+const renderTarget = document.querySelector(`#app`)
+
+ReactDOM.render(<App />, renderTarget)
+
